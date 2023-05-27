3f273e01c61d927fd93b681b2e7660198d7d1ee5
Emiliano Caballero <emiliano.caballero98@gmail.com>
2023-05-27 17:58:16 -0300

    Se habilitan la muestra de imagenes en productos y ademas se corrige la posicion de los elementos para q no rompan el esquema

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,13 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import Pagination from './tools/Pagination.jsx';
 
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { Box, Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export default function ImgMediaCard() {
@@ -42,7 +36,7 @@ export default function ImgMediaCard() {
     const cardsRender = currentItems.map(( p, index ) => 
         <Card 
             sx={{ 
-                width: 260,
+                width: 270, height: 295,
                 mr:2.5, mb: 2.5,
                 background: '#D0CD9466'
             }}
@@ -50,28 +44,21 @@ export default function ImgMediaCard() {
         >
             <CardMedia
                 component="img"
-                alt={p.prod_nombre}
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
+                height="150"
+                src={`https://dmartinablancohogar.com.ar/${p.prod_imagen}`}
+                alt={p.prod_imagen}
+                sx={{
+                    objectFit: "contain"
+                }}
             />
             <CardContent sx={{ display:'flex', justifyContent: 'center' }}>
-                <Typography variant="h6" component="div">
+                <Typography variant="body2" component="div" sx={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>
                     {p.prod_nombre}
                 </Typography>
             </CardContent>
-            <Box sx={{ 
-                display: 'flex', flexDirection:'column', alignItems: 'center'
-            }}>
-                <CardActions 
-                sx={{ 
-                background: '#241623', borderRadius: '10px' 
-                }}>
-                <Button 
-                    sx={{ 
-                    color: '#FFFFFF', px: '60px'
-                    }} 
-                    size="small"
-                >
+            <Box sx={{ display: 'flex', flexDirection:'column', alignItems: 'center' }}>
+                <CardActions sx={{ background: '#241623', borderRadius: '10px'}}>
+                <Button sx={{ color: '#FFFFFF', px: '60px' }} size="small">
                     <WhatsAppIcon sx={{ fontSize: '1rem', mb: '2px', mr: '5px' }}/>
                     <Typography sx={{ fontSize: '0.75rem' }} variant='caption'>
                         Whatsapp
